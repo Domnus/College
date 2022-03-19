b6e10aa9cfcd0d46455c38309d6f3816a25a1bcc
Bento Carlos <bcsilva49@gmail.com>
2022-03-19 16:57:47 -0300

    Componentes de Proprietarios

---
+++ 5º Semestre/Desenvolvimento de Sistemas de Informação/PetShopApp/Data/PetShopAppContext.cs
@@ -0,0 +1,17 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetShopApp.Models;
+
+    public class PetShopAppContext : DbContext
+    {
+        public PetShopAppContext (DbContextOptions<PetShopAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PetShopApp.Models.Proprietario> Proprietario { get; set; }
+    }
