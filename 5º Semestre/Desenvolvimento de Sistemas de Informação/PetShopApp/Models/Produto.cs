956cd1118ae23e59bd5cdbe33f69ff307b91fe69
Bento Carlos <bcsilva49@gmail.com>
2022-04-21 22:32:06 -0300

    Modelo de Produtos refatorado

--- 5º Semestre/Desenvolvimento de Sistemas de Informação/PetShopApp/Models/Produto.cs
+++ 5º Semestre/Desenvolvimento de Sistemas de Informação/PetShopApp/Models/Produto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System;
 
@@ -9,14 +10,20 @@ namespace PetShopApp.Models
 
         public string Nome {get; set;}
 
+        [Display(Name = "Descrição")]
         public string Descricao {get; set;}
 
+        [Display(Name = "Quantidade Estoque")]
         public int QuantidadeEstoque {get; set;}
 
         [Display(Name = "Preço de Compra")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public float PrecoCompra {get; set;}
 
         [Display(Name = "Preço de Venda")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public float PrecoVenda {get; set;}
 
 	}
